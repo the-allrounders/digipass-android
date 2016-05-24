b92c3ce3b35d770feedac0095cf25a73bbc1e41f
Bart Langelaan <langelaanbart@gmail.com>
2016-05-24 03:03:35 +0200

    Add Bluetooth enable request

--- app/src/main/java/com/digipass/android/BackgroundService.java
+++ app/src/main/java/com/digipass/android/BackgroundService.java
@@ -29,6 +29,7 @@ public class BackgroundService extends Service {
     private Handler h;
     private Notification.Builder notification;
     private BluetoothAdapter mBluetoothAdapter;
+    private PendingIntent pendingMainActivityIntent;
 
 
     @Override
@@ -46,8 +47,7 @@ public class BackgroundService extends Service {
         mBluetoothAdapter = ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter();
 
         // Create notification
-        Intent mainActivityIntent = new Intent(this, MainActivity.class);
-        PendingIntent pendingMainActivityIntent = PendingIntent.getActivity(this, 0, mainActivityIntent, 0);
+        pendingMainActivityIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
 
         notification = new Notification.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
@@ -71,10 +71,16 @@ public class BackgroundService extends Service {
 
     public void scan() {
         if(mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()){
-            notification.setContentText("Bluetooth disabled.");
+            notification.setContentText("Bluetooth disabled. Click here to enable.");
+            notification.setContentIntent(
+                    PendingIntent.getActivity(this, 0, new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), 0)
+            );
         }
         else {
             notification.setContentText("Bluetooth enabled.");
+            notification.setContentIntent(
+                    pendingMainActivityIntent
+            );
         }
 
         notification.setNumber(random.nextInt(100));
