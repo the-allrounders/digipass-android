69dd750898f7638e5119b8653e1e83f8ae8d92af
Ian Wensink <ianwensink@gmail.com>
2016-06-02 01:40:16 +0200

    Add Preference fragment

---
+++ app/src/main/java/com/digipass/android/helpers/PreferenceFragment.java
@@ -0,0 +1,77 @@
+package com.digipass.android.helpers;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.digipass.android.R;
+
+import java.util.ArrayList;
+
+public class PreferenceFragment extends DialogFragment {
+    ArrayList<Integer> mSelectedItems;
+
+    String[] options = {};
+    boolean[] values = {};
+
+    String title;
+
+    public void setData(String[] o, boolean[] v) {
+        options = o;
+        values = v;
+    }
+
+    public void setTitle(String t) {
+        title = t;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mSelectedItems = new ArrayList<>();  // Where we track the selected items
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Set the dialog title
+        builder.setTitle(title)
+                // Specify the list array, the items to be selected by default (null for none),
+                // and the listener through which to receive callbacks when items are selected
+                .setMultiChoiceItems(options, values,
+                        new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which,
+                                                boolean isChecked) {
+                                if (isChecked) {
+                                    // If the user checked the item, add it to the selected items
+                                    mSelectedItems.add(which);
+                                } else if (mSelectedItems.contains(which)) {
+                                    // Else, if the item is already in the array, remove it
+                                    mSelectedItems.remove(Integer.valueOf(which));
+                                }
+                            }
+                        })
+                // Set the action buttons
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK, so save the mSelectedItems results somewhere
+                        // or return them to the component that opened the dialog
+                        Log.d("dialog", "ok");
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.d("dialog", "cancel");
+                    }
+                });
+
+        return builder.create();
+    }
+}
