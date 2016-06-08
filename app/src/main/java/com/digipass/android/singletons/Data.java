203f241a33c258dd068aec85484e7bf861530058
Ian Wensink <ianwensink@gmail.com>
2016-06-08 17:57:01 +0200

    Little cleanup

--- app/src/main/java/com/digipass/android/singletons/Data.java
+++ app/src/main/java/com/digipass/android/singletons/Data.java
@@ -2,7 +2,6 @@ package com.digipass.android.singletons;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.util.Log;
 
 import com.digipass.android.objects.Preference;
 
@@ -28,7 +27,6 @@ public class Data {
         SharedPreferences pref_taxonomy_data = context.getSharedPreferences("preference_category_data", Context.MODE_PRIVATE);
         String pref_string = preference_data.getString("preferences_data", "[]");
         String tax_string = pref_taxonomy_data.getString("preference_category_data", "[]");
-        Log.d("key", key);
         try {
             JSONArray taxonomy_tree = new JSONArray(tax_string);
             preferences_list = getPreferencesList(key, taxonomy_tree, pref_string);
@@ -40,14 +38,12 @@ public class Data {
 
     private ArrayList<Preference> getPreferencesList(String key, JSONArray categories, String json) {
         ArrayList<Preference> preferences_list = new ArrayList<>();
-        JSONArray preferences = null;
-        Log.d("categories", categories.toString());
+        JSONArray preferences;
         try {
             preferences = new JSONArray(json);
             for(int n = 0; n < categories.length(); n++) {
                 JSONObject category = (JSONObject) categories.get(n);
                 JSONArray categoryParents = category.getJSONArray("parent");
-                Log.d("category", category.toString());
                 if (categoryParents.toString().contains("\""+ key +"\"") || (Objects.equals(key, "0") && categoryParents.length() == 0)) {
                     preferences_list.add(new Preference(category.getString("_id"), category.getString("title"), "", new JSONArray(), "group", category.getString("icon")));
                 }
@@ -59,11 +55,9 @@ public class Data {
                     preferences_list.add(new Preference(preference.getString("_id"), preference.getString("title"), preference.getString("description"), preference.getJSONArray("values"), "preference", ""));
                 }
             }
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
         return preferences_list;
     }
 
