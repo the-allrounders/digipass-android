1f3e36b4024e9e3b553381f422b86b5fd7f97b0e
Ian Wensink <ianwensink@gmail.com>
2016-06-21 22:07:27 +0200

    Code optimisation

--- app/src/main/java/com/digipass/android/SplashScreen.java
+++ app/src/main/java/com/digipass/android/SplashScreen.java
@@ -1,9 +1,7 @@
 package com.digipass.android;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -11,15 +9,9 @@ import com.digipass.android.singletons.API;
 
 public class SplashScreen extends AppCompatActivity {
 
-    // Splash screen timer
-//    private static int SPLASH_TIME_OUT = 3000;
-    private static int SPLASH_TIME_OUT = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final Context c = this;
-
     }
 
     @Override
@@ -29,9 +21,7 @@ public class SplashScreen extends AppCompatActivity {
         startActivity(new Intent(this, MainActivity.class));
 
         API api = API.getInstance(this);
-//        api.GetJSONResult();
-
-        final Context c = this;
+        api.GetJSONResult();
 
         if(api.username == null){
             Log.d("SplashScreen ", "Not logged in!");
@@ -39,20 +29,6 @@ public class SplashScreen extends AppCompatActivity {
         }
         else{
             Log.d("SplashScreen", "Logged in as " + api.username);
-
-            new Handler().postDelayed(new Runnable() {
-
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
-                @Override
-                public void run() {
-//                    Preference.ShowPreferenceList(c.getApplicationContext());
-//                    finish();
-                }
-            }, SPLASH_TIME_OUT);
         }
 
 
